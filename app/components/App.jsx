1751dd4d3608200a754b1ffc20f61cd87749fb68
Muehmik <heumin7@gmail.com>
2016-12-26 18:30:00 +0900

    docs(guide): updated fixed docs from Google Docs

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,18 +2,35 @@ import React from 'react';
 
 require('./App.css');
 
-export default () => <h1>Hello World</h1>;
+const marked = require('marked');
 
-/*
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      markdown: ''
+    };
+  }
+
+  handleChange(event) {
+    this.setState({ markdown: event.target.value });
   }
 
   render() {
     return (
-      <h1>Hello World</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1>Hello</h1>
+            <textarea onChange={this.handleChange.bind(this)} value={this.state.markdown} style={{width: 500}} className="form-control"/>
+          </div>
+          <div className="col-md-6">
+            <h1>Preview</h1>
+            <div dangerouslySetInnerHTML={{__html: marked(this.state.markdown)}}>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
-*/
